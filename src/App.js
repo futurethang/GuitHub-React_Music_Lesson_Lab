18ed8d8d380abe076eec09df6037ef84605bdf4c
Kevin <kphyde@gmail.com>
2019-02-22 07:42:19 -0800

    build react boilerplate, create simple components, lay out CSS grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <div class="grid-container">
+          <div class="VIDEO">VIDEO</div>
+          <div class="VIDEO-LIST">VIDEO LIST</div>
+          <div class="VIDEO-DETAILS">
+            <div class="NOTES">NOTES</div>
+          </div>
+          <div class="TOOLS">TOOLS</div>
+        </div>
       </div>
     );
   }
