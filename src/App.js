1a928176132941df012acaa0834b6ed7b2c0391b
Kevin <kphyde@gmail.com>
2019-03-01 18:09:56 -0800

    using custom adjustments to Bulma tiles to hack a seemingly working version of what I want.

--- src/App.js
+++ src/App.js
@@ -42,30 +42,95 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="navbar">
-          <div className="container">
-            <Header loadVideos={this.onLoadVideo} />
-          </div>
-        </div>
+        <Header loadVideos={this.onLoadVideo} />
 
-        <div className="section">
-          <div className="container">
-            <div className="VideoArea columns">
-              <VideoFrame video={this.state.featuredVideo} />
-              <VideoList videos={this.state.videos} />
+        <div
+          class="tile is-ancestor"
+          style={{ height: "400px", overflow: "scroll" }}
+        >
+          <div
+            class="tile is-8 is-parent"
+            style={{
+              position: "relative",
+              maxHeight: "200px",
+            }}
+          >
+            <div class="tile is-child box">
+              <p class="title">LEFTY</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                semper diam at erat pulvinar, at pulvinar felis blandit.
+                Vestibulum volutpat tellus diam, consequat gravida libero
+                rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc
+                dui porta orci, quis semper odio felis ut quam.
+              </p>
+              <p>
+                Suspendisse varius ligula in molestie lacinia. Maecenas varius
+                eget ligula a sagittis. Pellentesque interdum, nisl nec interdum
+                maximus, augue diam porttitor lorem, et sollicitudin felis neque
+                sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus
+                felis hendrerit sit amet. Aenean vitae gravida diam, finibus
+                dignissim turpis. Sed eget varius ligula, at volutpat tortor.
+              </p>
+              <p>
+                Integer sollicitudin, tortor a mattis commodo, velit urna
+                rhoncus erat, vitae congue lectus dolor consequat libero. Donec
+                leo ligula, maximus et pellentesque sed, gravida a metus. Cras
+                ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis
+                faucibus libero. Quisque non semper leo.
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="is-parent"
+            style={{
+              position: "relative",
+              maxHeight: "100%",
+              overflow: "auto"
+            }}
+          >
+            <div class="is-child box">
+              <p class="title">Three</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                semper diam at erat pulvinar, at pulvinar felis blandit.
+                Vestibulum volutpat tellus diam, consequat gravida libero
+                rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc
+                dui porta orci, quis semper odio felis ut quam.
+              </p>
+              <p>
+                Suspendisse varius ligula in molestie lacinia. Maecenas varius
+                eget ligula a sagittis. Pellentesque interdum, nisl nec interdum
+                maximus, augue diam porttitor lorem, et sollicitudin felis neque
+                sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus
+                felis hendrerit sit amet. Aenean vitae gravida diam, finibus
+                dignissim turpis. Sed eget varius ligula, at volutpat tortor.
+              </p>
+              <p>
+                Integer sollicitudin, tortor a mattis commodo, velit urna
+                rhoncus erat, vitae congue lectus dolor consequat libero. Donec
+                leo ligula, maximus et pellentesque sed, gravida a metus. Cras
+                ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis
+                faucibus libero. Quisque non semper leo.
+              </p>
             </div>
           </div>
         </div>
 
-        <div className="section">
-          <div className="container">
-            <div className="InfoAndTools">
+        {/* <div className="section">
+          <div className="container tile is-ancestor">
+            <div className="VideoArea tile">
+              <VideoFrame video={this.state.featuredVideo} />
+              <VideoList videos={this.state.videos} />
+            </div>
+            <div className="InfoAndTools tile">
               <VideoDetails />
               <Notes />
               <Tools />
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
